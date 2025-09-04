512f0871de7e6453e2cba8eb3bf556afa88d628f
EvgeniiaShirshikova <shirshikovae@gmail.com>
2025-09-04 10:50:57 -0400

    add hw3

---
+++ hw3/index.js
@@ -0,0 +1,17 @@
+const s = "Knowledge of built-in JavaScript methods speeds up a tester’s work."
+
+
+console.log(s.charAt(s.length -1))
+/*
+s.charAt(); //или с[0]
+s.charAt(s.length -1);
+s.substring(1, 4);
+s.substr(1, 3);
+s.slice(-3);
+s.indexOf(“l”);
+s.lastIndexOf(“l”);
+s.split(“of”);
+s.replace(“l”, “L”);
+s.replaceAll(“l”, “L”);
+s.toUpperCase();
+s.toLowerCase(); */
